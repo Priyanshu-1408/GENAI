90de16ea56ef060800301990a1c1565e7400cbb8
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-07-20 16:44:29 +0530

    using gemini api key

--- langgraph_learn/chat.py
+++ langgraph_learn/chat.py
@@ -1,15 +1,105 @@
+# from dotenv import load_dotenv
+# from typing_extensions import TypedDict
+# from typing import Annotated
+# from langgraph.graph.message import add_messages
+# from langgraph.graph import StateGraph, START, END
+# from langchain.chat_models import init_chat_model
+
+# load_dotenv()
+
+# llm = init_chat_model(
+#     model="gpt-4.1-mini",
+#     model_provider="openai"
+# )
+
+# class State(TypedDict):
+#     messages: Annotated[list, add_messages]
+
+# def chatbot(state: State):
+#     response = llm.invoke(state.get("messages"))
+#     return { "messages": [response] }
+
+# def samplenode(state: State):
+#     print("\n\nInside samplenode node", state)
+#     return { "messages": ["Sample Message Appended"] }
+
+# graph_builder = StateGraph(State)
+
+# graph_builder.add_node("chatbot", chatbot)
+# graph_builder.add_node("samplenode", samplenode)
+
+# graph_builder.add_edge(START, "chatbot")
+# graph_builder.add_edge("chatbot", "samplenode")
+# graph_builder.add_edge("samplenode", END)
+
+# graph = graph_builder.compile()
+
+# updated_state = graph.invoke(State({"messages": ["What is my name?"]}))
+# print("\n\nupdated_state", updated_state)
+
+# # (START) -> chatbot -> samplenode -> (END)
+
+# # state = { messages: ["Hey there"] }
+# # node runs: chatbot(state: ["Hey There"]) -> ["Hi, This is a message from ChatBot Node"]
+# # state = { "messages": ["Hey there", "Hi, This is a message from ChatBot Node"]  }
+
+
+
+from dotenv import load_dotenv
 from typing_extensions import TypedDict
 from typing import Annotated
 from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, START, END
+from google import genai
+import os
+
+load_dotenv()
+
+client = genai.Client(
+    api_key=os.getenv("GOOGLE_API_KEY")
+)
 
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
+
 def chatbot(state: State):
-    return {"message": ["hi, this is a message from chatbot node "]}    
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=state["messages"][-1]
+    )
+
+    return {
+        "messages": [response.text]
+    }
+
+
+def samplenode(state: State):
+    print("\nInside samplenode node")
+    print(state)
+
+    return {
+        "messages": ["Sample Message Appended"]
+    }
+
 
 graph_builder = StateGraph(State)
 
 graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("samplenode", samplenode)
+
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", "samplenode")
+graph_builder.add_edge("samplenode", END)
+
+graph = graph_builder.compile()
+
+updated_state = graph.invoke(
+    {
+        "messages": ["What is my name?"]
+    }
+)
+
+print("\nUpdated State:")
+print(updated_state)
